6c7380b825059bdaf9e786a02c9c03af14b864d7
Kazuhito Suda <kazuhito@fisuda.jp>
2019-11-14 20:04:06 +0900

    Update NPM dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -182,12 +182,12 @@ module.exports = function (grunt) {
 
     grunt.registerTask('test', [
         'eslint',
-//        'karma:operator'
+        //        'karma:operator'
     ]);
 
     grunt.registerTask('ci', [
         'eslint',
-//        'karma:operatorci',
+        //        'karma:operatorci',
         'coveralls'
     ]);
 
